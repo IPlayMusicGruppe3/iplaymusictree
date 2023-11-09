ca505a16b1667e351fcfa10081898ba006edeb5e
mido <101448145+midoch@users.noreply.github.com>
2023-11-09 12:12:15 +0100

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     fontFamily: {
       poppens: ["Poppins", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF1168",
+        secondary: "#341931",
+        additional: "#111625",
+        gradient: {
+          start: "#EE0979",
+          end: "#FF6A00",
+        },
+        custom: {
+          darkBlue: "#115793",
+          oceanBlue: "#00A1CB",
+          skyBlue: "#0AABEBE",
+          forestGreen: "#3A7634",
+          limeGreen: "#5EB11C",
+          gold: "#F2BC06",
+          orange: "#F18D05",
+          fieryRed: "#E54028",
+        },
+      },
+    },
   },
+  variants: {},
   plugins: [],
 };
