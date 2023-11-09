d2a1bfe4936fb5252f2561b783da49012a3def89
mido <101448145+midoch@users.noreply.github.com>
2023-11-09 10:27:56 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-slate-500">iPlayMusicTree</h1>
+    </>
+  );
+}
+
+export default App;
