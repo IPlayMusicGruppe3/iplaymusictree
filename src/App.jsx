194705f609c46e9b0f90e14e452a11f95e40bd0e
mido <101448145+midoch@users.noreply.github.com>
2023-11-09 13:43:28 +0100

    testing LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Welcome from "./pages/WelcomePage";
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <>
-      <Welcome />
+      <LoginPage />
     </>
   );
 }
