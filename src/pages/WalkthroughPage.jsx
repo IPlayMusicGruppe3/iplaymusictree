0ad00282b6c72da2e26077b85d7cbdc69bd4e1d7
mido <101448145+midoch@users.noreply.github.com>
2023-11-14 13:52:26 +0100

    Create WalkthroughPage.jsx

---
+++ src/pages/WalkthroughPage.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Heading from "../components/Heading";
+
+const WalkthroughPage = () => {
+  return (
+    <>
+      <Heading
+        HeadingText="Where Words Fail, Music Speaks"
+        paragraphText="This is some additional information."
+        className="text-blue-500"
+      />
+    </>
+  );
+};
+
+export default WalkthroughPage;
