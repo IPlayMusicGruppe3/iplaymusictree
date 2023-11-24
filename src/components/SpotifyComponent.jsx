f6439f2023192ddf80f7e8904d70093164ca20ce
waltherbaasch <U2444127@rts-365.dk>
2023-11-24 12:54:55 +0100

    added songs

--- src/components/SpotifyComponent.jsx
+++ src/components/SpotifyComponent.jsx
@@ -2,81 +2,86 @@ import React, { useState, useEffect } from "react";
 
 const CLIENT_ID = "e012db28aeb3453e861fd9d1658c5e58";
 const CLIENT_SECRET = "94f487ea6ce64f9eb1c0e77171cad5c7";
-const REDIRECT_URI = "http://localhost:5173/"; 
-
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=user-top-read&show_dialog=true`;
 
 const SpotifyComponent = () => {
-  const [token, setToken] = useState("");
   const [topTracks, setTopTracks] = useState([]);
 
   useEffect(() => {
-  
-    const getAccessTokenFromUrl = () => {
-      const hash = window.location.hash
-        .substring(1)
-        .split("&")
-        .reduce((initial, item) => {
-          if (item) {
-            const parts = item.split("=");
-            initial[parts[0]] = decodeURIComponent(parts[1]);
+    const fetchTopTracks = async () => {
+      try {
+        // Fetch access token using Client Credentials Flow
+        const tokenResponse = await fetch(
+          "https://accounts.spotify.com/api/token",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              Authorization: `Basic ${btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)}`,
+            },
+            body: "grant_type=client_credentials",
           }
-          return initial;
-        }, {});
-      return hash.access_token;
+        );
+
+        if (!tokenResponse.ok) {
+          console.error("Failed to fetch access token:", tokenResponse.status);
+          return;
+        }
+
+        const tokenData = await tokenResponse.json();
+        const accessToken = tokenData.access_token;
+
+        // Fetch top tracks using the obtained access token
+        const response = await fetch(
+          "https://api.spotify.com/v1/browse/new-releases?limit=10",
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setTopTracks(data.albums.items);
+        } else {
+          console.error("Failed to fetch top tracks:", response.status);
+        }
+      } catch (error) {
+        console.error("Error fetching top tracks:", error.message);
+      }
     };
 
-   
-    const accessToken = getAccessTokenFromUrl();
-
-    if (accessToken) {
-
-      setToken(accessToken);
-
-   
-      fetchTopTracks(accessToken);
-    } else {
-   
-      window.location.href = AUTH_URL;
-    }
+    fetchTopTracks();
   }, []);
 
-  const fetchTopTracks = async (accessToken) => {
-    try {
-      const response = await fetch(
-        "https://api.spotify.com/v1/me/top/tracks?limit=10",
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
-
-      if (response.ok) {
-        const data = await response.json();
-        setTopTracks(data.items);
-      } else {
-        console.error("Failed to fetch top tracks:", response.status);
-      }
-    } catch (error) {
-      console.error("Error fetching top tracks:", error.message);
-    }
-  };
-
-  const handlePlayTrack = (trackUri) => {
-   
-    console.log("Play track:", trackUri);
-  };
-
   return (
-    <div>
-      <h1>Top 10 Songs on Spotify</h1>
-      <ul>
+    <div className="container mx-auto my-8">
+      <h1 className="text-3xl font-bold mb-4">Top 10 Songs on Spotify</h1>
+      <ul className="space-y-4">
         {topTracks.map((track) => (
-          <li key={track.id}>
-            {track.name} -{" "}
-            {track.artists.map((artist) => artist.name).join(", ")}
-            <button onClick={() => handlePlayTrack(track.uri)}>Play</button>
+          <li
+            key={track.id}
+            className="flex items-center justify-between border-b pb-2"
+          >
+            <div className="flex items-center">
+              <iframe
+                title={track.name}
+                src={`https://open.spotify.com/embed/album/${track.id}`}
+                width="300"
+                height="380"
+                frameBorder="0"
+                allowtransparency="true"
+                allow="encrypted-media"
+                className="mr-4"
+              ></iframe>
+              <div>
+                <p className="font-semibold">{track.name}</p>
+                <p className="text-gray-600">
+                  {track.artists.map((artist) => artist.name).join(", ")}
+                </p>
+              </div>
+            </div>
+            <p className="text-gray-600">{/* Track length goes here */}</p>
           </li>
         ))}
       </ul>
